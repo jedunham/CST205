42ff13e226d30c2d4bb5816eff62157e9a5469f5
Jennifer Dunham <jedunham@csumb.edu>
2014-11-24 21:36:06 -0800

    Drastically simplified border code using external functions for rotate and whCopy

--- CSUMBerize.py
+++ CSUMBerize.py
@@ -22,132 +22,89 @@ def addBorder(pic):
   bottomRight = makePicture("C:\Users\whitenebula\Documents\School\CSUMB\CST 205\Module 4\midterm\sourceimages\\bottomright.jpg")
   topLeft = makePicture("C:\Users\whitenebula\Documents\School\CSUMB\CST 205\Module 4\midterm\sourceimages\\topleft.jpg")
   topRight = makePicture("C:\Users\whitenebula\Documents\School\CSUMB\CST 205\Module 4\midterm\sourceimages\\topright.jpg")
-  borderDim=getHeight(border) #square image
+  
+  #declare variables
+  borderDim=getHeight(border)     #square image
   cornerDim=getHeight(bottomLeft) #square image
+  
   #bottom border
   targetX = 0
   targetY = getHeight(pic)-getHeight(border)
   for i in range(int(getWidth(pic)/borderDim)): #int to prevent image falling out of range
-    for x in range (0, getWidth(border)):
-      for y in range (0, getHeight(border)):
-        color = getColor(getPixel(border, x, y))
-        if distance(color, white) > 75.0:
-          setColor(getPixel(pic, x+targetX, y+targetY), color)
+    pic = whCopy(border, pic, targetX, targetY, 75.0)
     targetX = targetX + borderDim
-  #rotate wave border for top border
-  border180 = makeEmptyPicture(getWidth(border), getHeight(border))
-  for x in range(0, getWidth(border)):
-    for y in range(0, getHeight(border)):
-      px = getColor(getPixel(border, x, y))
-      setColor(getPixel(border180, getWidth(border)-x-1, getHeight(border)-y-1), px)
-  #topborder
-  targetX = 0
-  targetY = 0
-  for i in range(0, int(getWidth(pic)/borderDim)): #int to prevent image falling out of range
-    for x in range(0, getWidth(border180)):
-      for y in range(0, getHeight(border180)):
-        color = getColor(getPixel(border180, x, y))
-        if distance(color, white) > 75.0:
-          setColor(getPixel(pic, x+targetX, y+targetY), color)
-    targetX = targetX + borderDim
-  #rotate wave border for right border
-  borderCW = makeEmptyPicture(getHeight(border), getWidth(border))
-  for x in range(0, getWidth(border)):
-    for y in range(0, getHeight(border)):
-      px = getColor(getPixel(border, x, y))
-      setColor(getPixel(borderCW, y, getWidth(border)-x-1), px)
+  
   #rightborder
+  borderRight = rotatePic(border)
   targetX = getWidth(pic) - borderDim
   targetY = 0
   for i in range(0, int(getHeight(pic)/borderDim)): #int to prevent image falling out of range
-    for x in range(0, getWidth(borderCW)):
-      for y in range(0, getHeight(borderCW)):
-        color = getColor(getPixel(borderCW, x, y))
-        if distance(color, white) > 75.0:
-          setColor(getPixel(pic, x+targetX, y+targetY), color)
+    pic = whCopy(borderRight, pic, targetX, targetY, 75.0)
     targetY = targetY + borderDim
-  #rotate wave border for right border
-  borderCCW = makeEmptyPicture(getHeight(border), getWidth(border))
-  for x in range(0, getWidth(border)):
-    for y in range(0, getHeight(border)):
-      px = getColor(getPixel(border, x, y))
-      setColor(getPixel(borderCCW, getHeight(border)-y-1, x), px)
+  
+  #topborder
+  borderTop = rotatePic(borderRight)
+  targetX = 0
+  targetY = 0
+  for i in range(0, int(getWidth(pic)/borderDim)): #int to prevent image falling out of range
+    pic = whCopy(borderTop, pic, targetX, targetY, 75.0)
+    targetX = targetX + borderDim
+  
   #leftborder
+  borderLeft = rotatePic(borderTop)
   targetX = 0
   targetY = 0
   for i in range(0, int(getHeight(pic)/borderDim)): #int to prevent image falling out of range
-    for x in range(0, getWidth(borderCCW)):
-      for y in range(0, getHeight(borderCCW)):
-        color = getColor(getPixel(borderCCW, x, y))
-        if distance(color, white) > 75.0:
-          setColor(getPixel(pic, x+targetX, y+targetY), color)
+    pic = whCopy(borderLeft, pic, targetX, targetY, 75.0)
     targetY = targetY + borderDim
+  
   #add corner images
   #top left
   targetX = 0
   targetY = 0
-  for x in range(0, getWidth(topLeft)):
-    for y in range(0, getHeight(topLeft)):
-      color = getColor(getPixel(topLeft, x, y))
-      if distance(color, white) > 200.0:
-        setColor(getPixel(pic, x+targetX, y+targetY), color)
+  pic = whCopy(topLeft, pic, targetX, targetY, 200.0)
   #top right
   targetX = getWidth(pic)-cornerDim
   targetY = 0
-  for x in range(0, getWidth(topRight)):
-    for y in range(0, getHeight(topRight)):
-      color = getColor(getPixel(topRight, x, y))
-      if distance(color, white) > 200.0:
-        setColor(getPixel(pic, x+targetX, y+targetY), color)
+  pic = whCopy(topRight, pic, targetX, targetY, 200.0)
   #bottom left
   targetX = 0
   targetY = getHeight(pic)-cornerDim
-  for x in range(0, getWidth(bottomLeft)):
-    for y in range(0, getHeight(bottomLeft)):
-      color = getColor(getPixel(bottomLeft, x, y))
-      if distance(color, white) > 200.0:
-        setColor(getPixel(pic, x+targetX, y+targetY), color)
+  pic = whCopy(bottomLeft, pic, targetX, targetY, 200.0)
   #bottom right
   targetX = getWidth(pic)-cornerDim
   targetY = getHeight(pic)-cornerDim
-  for x in range(0, getWidth(bottomRight)):
-    for y in range(0, getHeight(bottomRight)):
-      color = getColor(getPixel(bottomRight, x, y))
-      if distance(color, white) > 200.0:
-        setColor(getPixel(pic, x+targetX, y+targetY), color)
+  pic = whCopy(bottomRight, pic, targetX, targetY, 200.0)
   return pic
 
 def CSUMBBlend(pic):  
-  #blends CSUMB school colors into image in 3 vertical stripes
-  bayBlue = makeColor(0, 47, 93)
-  goldenSand = makeColor(132, 114, 72)
-  valleyGreen = makeColor(0, 120, 86)
+  """blends CSUMB school colors into image in 3 vertical stripes"""
   amount = 0.5
   for x in range(0, getWidth(pic)/3):
     for y in range(0, getHeight(pic)):
       pixel = getPixel(pic, x, y)
       px = getColor(pixel)
-      newBBRed = 0*amount + getRed(pixel)*(1-amount)
-      newBBGreen = 47*amount + getGreen(pixel)*(1-amount)
-      newBBBlue = 93*amount + getBlue(pixel)*(1-amount)
+      newBBRed = 0*amount + getRed(pixel)*(1-amount)      #r = 0 in Bay Blue
+      newBBGreen = 47*amount + getGreen(pixel)*(1-amount) #g = 47 in Bay Blue
+      newBBBlue = 93*amount + getBlue(pixel)*(1-amount)   #b = 93 in Bay Blue
       newColor = makeColor(newBBRed, newBBGreen, newBBBlue)
       setColor(pixel, newColor)
   for x in range(getWidth(pic)/3, getWidth(pic)*2/3):
     for y in range(0, getHeight(pic)):
       pixel = getPixel(pic, x, y)
       px = getColor(pixel)
-      newGSRed = 132*amount + getRed(pixel)*(1-amount)
-      newGSGreen = 114*amount + getGreen(pixel)*(1-amount)
-      newGSBlue = 72*amount + getBlue(pixel)*(1-amount)
+      newGSRed = 132*amount + getRed(pixel)*(1-amount)     #r = 132 in Golden Sand
+      newGSGreen = 114*amount + getGreen(pixel)*(1-amount) #g = 114 in Golden Sand
+      newGSBlue = 72*amount + getBlue(pixel)*(1-amount)    #b = 72 in Golden Sand
       newColor = makeColor(newGSRed, newGSGreen, newGSBlue)
       setColor(pixel, newColor)
   for x in range(getWidth(pic)*2/3, getWidth(pic)):
     for y in range(0, getHeight(pic)):
       pixel = getPixel(pic, x, y)
       px = getColor(pixel)
-      newVGRed = 0*amount + getRed(pixel)*(1-amount)
-      newVGGreen = 120*amount + getGreen(pixel)*(1-amount)
-      newVGBlue = 86*amount + getBlue(pixel)*(1-amount)
+      newVGRed = 0*amount + getRed(pixel)*(1-amount)       #r = 0 in Valley Green
+      newVGGreen = 120*amount + getGreen(pixel)*(1-amount) #g = 120 in Valley Green
+      newVGBlue = 86*amount + getBlue(pixel)*(1-amount)    #b = 86 in Valley Green
       newColor = makeColor(newVGRed, newVGGreen, newVGBlue)
       setColor(pixel, newColor)
   return pic
@@ -161,9 +118,28 @@ def dkbCopy(source, target, targetX, targetY):
       if distance(color, dkblue) > 50.0:
         setColor(getPixel(target, x+targetX, y+targetY), color)
   return target  
-  
+
+def whCopy(source, target, targetX, targetY, dist):
+  """copy a picture onto a target picture excluding white"""
+  for x in range (0, getWidth(source)):
+    for y in range (0, getHeight(source)):
+      color = getColor(getPixel(source, x, y))
+      if distance(color, white) > dist:
+        setColor(getPixel(target, x+targetX, y+targetY), color)
+  return target    
+
+def rotatePic(pic):
+  """rotates pic 90° counterclockwise"""
+  newpic = makeEmptyPicture(getHeight(pic), getWidth(pic))
+  for x in range (0, getWidth(pic)):
+    for y in range (0, getHeight(pic)):
+      px = getColor(getPixel(pic, x, y))
+      setColor(getPixel(newpic, y, getWidth(pic)-x-1), px)
+  return newpic
+
 #Text
 def text(pic):
+  """Adds "CSUMB" text to image"""
   #define style for images with width < 300
   if getWidth(pic) < 300:
     text = makeStyle(sansSerif, bold, 20)
